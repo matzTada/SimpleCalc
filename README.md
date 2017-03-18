03ecdd9a206bef7937c4390da84c2faff0600c96
matzTada <matz@west.sd.keio.ac.jp>
2017-03-18 18:07:56 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+#Calc
+some exlanations...  
+
+
+###Pin assign
+
+Arduino|Other  
+--- | ---   
+0 (RX) | Keypad 1 (row 0)    
+1 (TX) | Keypad 2 (row 1)  
+2 | 7seg4dig display 1  
+3 | 7seg4dig display 2   
+4 | 7seg4dig display 3   
+5 | 7seg4dig display 4   
+6 | 7seg4dig display 5   
+7 | 7seg4dig display 6 (via Resistor)      
+8 | 7seg4dig display 7    
+9 | 7seg4dig display 8 (via Resistor)    
+10 | 7seg4dig display 9 (via Resistor)    
+11 | 7seg4dig display 10   
+12 | 7seg4dig display 11   
+13 | 7seg4dig display 12 (via Resistor)    
+A0 (14) | Keypad 7 (col 3)  
+A1 (15) | Keypad 6 (col 2)  
+A2 (16) | Keypad 5 (col 1)  
+A3 (17) | Keypad 4 (col 0)  
+A4 (18) | Keypad 3 (row 3)  
+A5 (19) | Keypad 2 (row 2)  
+
+
+<a href="http://www.youtube.com/watch?feature=player_embedded&v=YOUTUBE_MOVIE_ID_HERE
+" target="_blank"><img src="http://img.youtube.com/vi/YOUTUBE_MOVIE_ID_HERE/0.jpg " 
+alt="movie on youtube" width=40% border="10" /></a>  
+[movie on youtube](https://www.youtube.com/watch?v=YOUTUBE_MOVIE_ID_HERE)  
+you must change 3 places "YOUTUBE_MOVIE_ID_HERE" to your same movie id.
+
+Overview of this project.
+
+## Proparation
+Explanation.  
+
+* <https://matztada.github.io>  
+* [link](https://matztada.github.io)
+
+## Images
+
+<a><img src="https://matztada.github.io/images/logo.jpg" alt="" width=50%></a>  
---
+++ sketch_mar02cFlashCalculation/sketch_mar02cFlashCalculation.ino
@@ -0,0 +1,146 @@
+/*4x4 Matrix Keypad connected to Arduino
+  This code prints the key pressed on the keypad to the serial port*/
+
+#include <Keypad.h>
+
+const byte numRows = 4; //number of rows on the keypad
+const byte numCols = 4; //number of columns on the keypad
+
+//keymap defines the key pressed according to the row and columns just as appears on the keypad
+char keymap[numRows][numCols] =
+{
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+//Code that shows the the keypad connections to the arduino terminals
+byte rowPins[numRows] = {0, 1, 19, 18}; //from LEFT Rows 0 to 3
+byte colPins[numCols] = {17, 16, 15, 14}; //Columns 0 to 3
+
+//initializes an instance of the Keypad class
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+
+//============================7seg4digits
+const int cathode_pins[] = {12, 8, 5, 3, 2, 11, 6};    // {1,2,3,4,5,6,7}th from LEFT (a,b,c,d,e,f,g,df) アノードに接続するArduinoのピン
+const int anode_pins [] = {7, 9, 10, 13};  // {1, 2, 3, 4}th from RIGHT (dig4, dig3, dig2, dig1) カソードに接続するArduinoのピン
+const int number_of_cathode_pins = sizeof(cathode_pins) / sizeof(cathode_pins[0]);
+const int number_of_anode_pins = sizeof(anode_pins) / sizeof(anode_pins[0]);
+
+const int digits[] = {
+  0b00111111, // 0
+  0b00000110, // 1
+  0b01011011, // 2
+  0b01001111, // 3
+  0b01100110, // 4
+  0b01101101, // 5
+  0b01111101, // 6
+  0b00100111, // 7
+  0b01111111, // 8
+  0b01101111, // 9
+};
+
+// 数字(n)を表示する
+void display_number (int n) {
+  for (int i = 0; i < number_of_cathode_pins; i++) {
+    digitalWrite(cathode_pins[i], digits[n] & (1 << i) ? LOW : HIGH);
+  }
+}
+
+void clear_segments() {
+  for (int j = 0; j < number_of_cathode_pins; j++) {
+    digitalWrite(cathode_pins[j], HIGH);
+  }
+}
+
+void display_numbers (int n) {
+  for (int i = 0; i < number_of_anode_pins; i++) {
+    digitalWrite(anode_pins[i], HIGH);
+    display_number(n % 10); // 最後の一桁を表示する
+    delay(1);
+    clear_segments();
+    digitalWrite(anode_pins[i], LOW);
+    n = n / 10; // 10で割る
+  }
+}
+
+int num = 0;
+String currentStr = "0000";
+int operand = 0;
+char ope = ' ';
+
+void setup()
+{
+  //Serial.begin(9600);
+
+  //============================7seg4digits
+  for (int i = 0; i < number_of_cathode_pins; i++) {
+    pinMode(cathode_pins[i], OUTPUT);  // cathode_pinsを出力モードに設定する
+  }
+  for (int i = 0; i < number_of_anode_pins; i++) {
+    pinMode(anode_pins[i], OUTPUT);  // anode_pinsを出力モードに設定する
+    digitalWrite(anode_pins[i], LOW);
+  }
+}
+
+//If key is pressed, this key is stored in 'keypressed' variable
+//If key is not equal to 'NO_KEY', then this key is printed out
+//if count=17, then count is reset back to 0 (this means no key is pressed during the whole keypad scan process
+void loop()
+{
+  char keypressed = myKeypad.getKey();
+  if (keypressed != NO_KEY)
+  {
+    if ('0' <= keypressed && keypressed <= '9') {
+      currentStr += keypressed;
+      num = currentStr.substring(currentStr.length() - 4, currentStr.length()).toInt();
+    }
+    else {
+      switch (keypressed) {
+        case 'A':
+          operand = num;
+          ope = '+';
+          break;
+        case 'B':
+          operand = num;
+          ope = '-';
+          break;
+        case 'C':
+          operand = num;
+          ope = '*';
+          break;
+        case 'D':
+          operand = num;
+          ope = '/';
+          break;
+        case '#':
+          switch (ope) {
+            case '+':
+              num = operand + num;
+              break;
+            case '-':
+              num = operand - num;
+              break;
+            case '*':
+              num = operand * num;
+              break;
+            case '/':
+              num = operand / num;
+              break;
+            default:
+              break;
+          }
+          break;
+        case '*':
+          num = 0;
+          operand = 0;
+          break;
+      }
+      currentStr = "0000";
+    }
+    //    Serial.print(keypressed);
+  }
+  display_numbers(num);
+}
